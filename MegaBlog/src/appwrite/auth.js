ef3feb1f6e51b42fa3dd3da733b599dbce8d4ccf
Shekhu04 <gshikhar04@gmail.com>
2024-07-14 00:32:37 +0530

    add some functionalities

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -17,6 +17,7 @@ export class AuthService{
         const userAccount = await this.account.create(ID.unique(),email,password,name);
         if(userAccount) {
             //call another method
+            return this.login({email,password});
         } else{
             return userAccount;
         }
@@ -28,6 +29,23 @@ export class AuthService{
    async login({email,password}){
     try {
         return await this.account.createEmailSession(email,password);
+    } catch (error) {
+        throw error;
+    }
+   }
+
+   async getCurrentUser(){
+    try{
+        return await this.account.get();
+    } catch(error){
+        throw error;
+    }
+    return null;
+   }
+
+   async logout(){
+    try {
+        await this.account.deleteSessions();
     } catch (error) {
         throw error;
     }
